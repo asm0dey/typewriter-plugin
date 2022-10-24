efe54dd8113866b09060c3712e6372d97db623f8
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-10-24 21:28:23 +0200

    code review

--- src/main/kotlin/com/github/asm0dey/typewriterplugin/TypeWriterBundle.kt
+++ src/main/kotlin/com/github/asm0dey/typewriterplugin/TypeWriterBundle.kt
@@ -5,9 +5,9 @@ import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
-private const val BUNDLE = "messages.MyBundle"
+private const val BUNDLE = "messages.TypeWriterBundle"
 
-object MyBundle : DynamicBundle(BUNDLE) {
+object TypeWriterBundle : DynamicBundle(BUNDLE) {
 
     @Suppress("SpreadOperator")
     @JvmStatic
---
+++ src/main/kotlin/com/github/asm0dey/typewriterplugin/TypeWriterConstants.kt
@@ -0,0 +1,6 @@
+package com.github.asm0dey.typewriterplugin
+
+object TypeWriterConstants {
+
+    const val defaultDelay = 100
+}
